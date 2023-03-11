9b7ecd3579a97378182fc0fe121277cf06c438ab
Arbaz Khan <arbazkhan5300@gmail.com>
2023-03-12 02:16:19 +0500

    modified code in main function

--- ai.py
+++ ai.py
@@ -2,6 +2,10 @@ from random import randint
 import random
 
 #graph
+mutation_rate = 3
+total_cities = 6
+population_size = 5
+total_generations = 100
 
 cities = {"P": "Peshawar",
           "I": "Islamabad",
@@ -9,8 +13,6 @@ cities = {"P": "Peshawar",
           "K": "Karachi",
           "L": "Lahore",
           "M": "Multan"}
-
-total_cities = 6
 
 position = {"P": 0,
             "I": 1,
@@ -26,7 +28,7 @@ graph = [[0, 20, float('inf'), 250, float('inf'), 100],
          [float('inf'), 40, float('inf'), 180, 0, 90],
          [100, float('inf'), float('inf'), float('inf'), 90, 0]]
 
-mutation_rate = 3
+
 
 class genome:
   def __init__(self, _path, _fitness):
@@ -73,13 +75,25 @@ def tournament_selection(population, number):
     return population[0:number] 
 
 
-def crossover(offspring, i, j, path):
-
+def ordered_crossover(parent_1, parent_2, crossover_start, crossover_end):
+    
+    p1_path = list(parent_1.path[0:len(parent_1.path) - 1])
+    p2_path = list(parent_2.path[0:len(parent_2.path) - 1])
+    offspring = []
+     
+    for i in range(len(p1_path)):
+        offspring.extend("x")
+    
+    offspring[crossover_start:crossover_end] = p1_path[crossover_start:crossover_end]
+    
+    i = crossover_end
+    j = 0
+     
     while 'x' in offspring:
     
-        if i < len(path):
-            if  path[i] not in offspring and j < len(offspring):
-                offspring[j] = path[i]
+        if i < len(p2_path):
+            if  p2_path[i] not in offspring and j < len(offspring):
+                offspring[j] = p2_path[i]
                 j += 1
             i += 1
         else:
@@ -88,29 +102,9 @@ def crossover(offspring, i, j, path):
     offspring.extend(offspring[0])
     path = "".join(offspring)
     
-    return path
-
-def ordered_crossover(parent_1, parent_2, crossover_start, crossover_end):
+    new_genome = genome(path, fitness(path))
     
-    p1_path = list(parent_1.path[0:len(parent_1.path) - 1])
-    p2_path = list(parent_2.path[0:len(parent_2.path) - 1])
-    
-    offspring_1 = []
-    offspring_2 = []
-     
-    for i in range(len(p1_path)):
-        offspring_1.extend("x")
-        offspring_2.extend("x")
-    
-    offspring_1[crossover_start:crossover_end] = p1_path[crossover_start:crossover_end]
-    offspring_2[crossover_start:crossover_end] = p2_path[crossover_start:crossover_end]
-     
-    offspring_1 = crossover(offspring_1, crossover_end, 0, p2_path)
-    offspring_2 = crossover(offspring_2, crossover_end, 0, p1_path)
-    
-    offsprings = (genome(offspring_1, fitness(offspring_1)), genome(offspring_2, fitness(offspring_2)))
-    
-    return offsprings
+    return new_genome
 
 def mutation(chromosome):
     
@@ -133,16 +127,27 @@ def mutation(chromosome):
         
     new_offspring = genome(path + path[0], fitness(path)) 
     return new_offspring
+
+
+def total_population_score(population):
+    
+    score = 0
+    
+    for individual in population:
+        score += individual.fitness
+        
+    return score
      
                 
 def main():
+    
     g1 = genome("PISKLMP", 0)
     g2 = genome("LISKPML", 0)
     g3 = genome("ILMPKSI", 0)
     g4 = genome("KPMLISK", 0)
     g5 = genome("SIPMLKS", 0)
 
-    initial_population = []
+    population = []
 
     genomes = [g1, g2, g3, g4, g5]
 
@@ -150,20 +155,45 @@ def main():
         
         i.fitness = fitness(i.path)
         
-        initial_population.append(i)
+        population.append(i)
         
-    selected = tournament_selection(initial_population, 3)
+    score = total_population_score(population)
+    
+    generations = 1
+    
+    while (generations <= total_generations) and (score > 600):
+    
+        print("Generation: ", generations)
+    
+        fittest = tournament_selection(population, 3)
+        
+        for i in fittest:
+            print("Fittest\n", i.path, i.fitness)
+            
+            
+        new_generation = []
+        possible_parents = [0, 1, 2]
+        
+        for i in range(population_size-len(fittest)):
+            
+            parent_1, parent_2 = random.sample(possible_parents, 2)
+            new_offspring = ordered_crossover(fittest[parent_1], fittest[parent_2], 2, 5)
+            mutated_new_offspring = mutation(new_offspring)
+            new_generation.append(mutated_new_offspring)
+            
+        for i in range(len(fittest)):
+            mutated_new_offspring = mutation(fittest[i])
+            new_generation.append(mutated_new_offspring)
 
-    for i in range(3):    
-        print(selected[i].path , selected[i].fitness)
-         
-    new_genome = ordered_crossover(initial_population[0], initial_population[1], 2, 5)
-    
-    print (new_genome)
-    
-    mutated_offspring = mutation(initial_population[0])
-    
-    print(mutated_offspring.path)
+        for i in new_generation:
+            print("New Generation\n", i.path, i.fitness)
+            
+        population = new_generation
+        score = total_population_score(population)
+        generations += 1
+
+
+
         
 if __name__ == "__main__":
     main()
